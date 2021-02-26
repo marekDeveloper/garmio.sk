e9237768bb73aae2f6187d790130b33447044bf3
Marek <pisch.marek@gmail.com>
2021-02-25 23:04:50 -0700

    Some tests

---
+++ garmio.sk/src/pages/page-3.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ThirdPage = () => (
+  <Layout>
+    <SEO title="Page Three" />
+    <h1>Page 3</h1>
+    <p>You just hit third page!</p>
+  </Layout>
+)
+
+export default ThirdPage
